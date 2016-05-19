94d4fa8a6345cb3b668c11354347f2de6197e570
Melanie Burger <melanie.brgr@gmail.com>
2016-05-19 13:18:14 -0400

    display imgs from Flickr via AJAX request

--- app.js
+++ app.js
@@ -1,37 +1,41 @@
 angular.module('ngFlickr', ['ngMessages'])
-	.controller('formCtrl', function($scope, $http) {
-// Key: 06cc701f07dcdba806fd64bf8b0dcf46
-// Secret: 275c17ce4b0adad5
+	.controller('formCtrl', function($rootScope, $scope, $http) {
 
 		var url = 'https://api.flickr.com/services/rest';
-		var secret = '275c17ce4b0adad5';
 		var api_key = '06cc701f07dcdba806fd64bf8b0dcf46';
-		var api_sig =  secret + 'api_key' + api_key + 'perms' + 'read';
-		api_sig = calcMD5(api_sig);
-
+		$scope.model = {
+			numResults: 10
+		}
+		// var secret = '275c17ce4b0adad5';
+		// var api_sig =  secret + 'api_key' + api_key + 'perms' + 'read';
+		// api_sig = calcMD5(api_sig);
 
 		$scope.getFlickrData = function() {
-
-//https://www.flickr.com/services/api/flickr.photos.search.html
+			$scope.model.message = 'Searching instagram for photos tagged with '+$scope.model.tag;
 			$http({
 			    url: url,
 			    method: "GET",
 			    params: {
 						method: 'flickr.photos.search',
-						tags: 'cat',
+						tags: $scope.model.tag,
 						api_key: api_key,
 						format: 'json',
 						//api_sig: api_sig,
-						jsoncallback:'JSON_CALLBACK'
+						//jsoncallback:'JSON_CALLBACK',
+						per_page: $scope.model.numResults
 					}
 			 })
 			.then(function(data, status, headers, config) {
-				console.log(data);
+				function jsonFlickrApi(data) {
+					console.log(data);
+					$rootScope.data = data;
+				}
+				eval(data.data);
+				$scope.model.message = 'We found '+$scope.model.numResults+' results for '+$scope.model.tag+'.';
+				$scope.model.tag = '';
 			}, function(data, status, headers, config) {
 				console.log('Failure :(');
 			});
 		};
-
-		console.log(url+'?api_key='+api_key+'&api_sig='+api_sig);
 
 	});
